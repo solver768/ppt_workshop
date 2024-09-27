78a341c5a3c931e9628ebd9ac7f563291f2eb072
P.V.S.L.Deepthi <161279349+solver768@users.noreply.github.com>
2024-09-27 12:13:25 +0530

    Create week2.py

---
+++ week2.py
@@ -0,0 +1,73 @@
+#in print function will print the string data within the quotation marks
+#program to print timetable
+print("____________________________________________________________________________")
+print("|Day |5:00  |6:00   |7:00    |7:30     |8:00   |8:30|9:00|noon|5:00|6:30|7:30|8:00|8:30|10:00| ")
+print("|----|------|-------|--------|---------|-------|---------------------------|")
+print("|Mon |wakeup|freshup|getReady|Breakfast|library|                        ")
+print("|----|------|-------|--------|---------|-------|-----------------|")
+print("|Tue |wakeup|freshup|getReady|Breakfast|library|
+print("|----|------|-------|--------|---------|-------|------------------|")
+print("|Wed |wakeup|freshup|getReady|Breakfast|library|
+print("-----|------|-------|--------|---------|-------|------------------|")
+print("|Thur|wakeup|freshup|getReady|Breakfast|library|
+print("|----|------|-------|--------|---------|-------|------------------|")
+print("|Fri |wakeup|freshup|getReady|Breakfast|library|
+print("|----|------|-------|--------|---------|-------|------------------|")
+print("|Sat |wakeup|freshup|getReady|Breakfast|library|
+print("|----|------|-------|--------|---------|-------|------------------|")
+print("|Sun |sleep |wakeup |freshup |getReady |breakfast|
+print("|____|______|_______|________|_________|_______|________________________________")
+
+#comma in print function give a single space in the ouput displays on console
+
+#print normal 5 table using numbers
+#here print statement has only single datatype to concate and display on the console
+print(5,1,5*1)
+print(5,2,5*2)
+print(5,3,5*3)
+print(5,4,5*4)
+print(5,5,5*5)
+print(5,6,5*6)
+print(5,7,5*7)
+print(5,8,5*8)
+print(5,9,5*9)
+print(5,10,5*10)
+
+output:
+5 1 5
+5 2 10
+5 3 15
+5 4 20
+5 5 25
+5 6 30
+5 7 35
+5 8 40
+5 9 45
+5 10 50
+
+
+
+#print normal 5 table with (*/X)
+#here print statement has "2" types of datatypes to concate and display on console
+print(5,"x",1,"=",5*1)
+print(5,"x",2,"=",5*2)
+print(5,"x",3,"=",5*3)
+print(5,"x",4,"=",5*4)
+print(5,"x",5,"=",5*5)
+print(5,"x",6,"=",5*6)
+print(5,"x",7,"=",5*7)
+print(5,"x",8,"=",5*8)
+print(5,"x",9,"=",5*9)
+print(5,"x",10,"=",5*10)
+
+output:
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
